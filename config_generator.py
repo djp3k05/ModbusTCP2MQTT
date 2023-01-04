286219f0b0266a65571b4c0a03680f8d2010b9be
djp3k05 <79905373+djp3k05@users.noreply.github.com>
2023-01-04 16:51:57 +0200

    Update config_generator.py

--- config_generator.py
+++ config_generator.py
@@ -91,10 +91,17 @@ sensors.append({'name': "Daily Generation", 'sensor_type': "sensor",
 'register': "daily_power_yields", 'dev_class': "energy",
 'state_class': "total_increasing"})
 
+sensors.append({'name': "Total Generation", 'sensor_type': "sensor",
+'register': "total_power_yields", 'dev_class': "energy",
+'state_class': "total_increasing"})
+
 sensors.append({'name': "Active Power", 'sensor_type': "sensor",
 'register': "total_active_power", 'dev_class': "power",
 'state_class': "measurement"})
 
+sensors.append({'name': "Total Apparent Power", 'sensor_type': "sensor",
+'register': "total_apparent_power", 'dev_class': "power",
+'state_class': "measurement"})
 
 
 #Standard Level
@@ -205,11 +212,47 @@ if options['inverter']['level'] >= 2:
     sensors.append({'name': "Total Reactive Power", 'sensor_type': "sensor",
     'register': "total_reactive_power", 'dev_class': "power",
     'state_class': "measurement"})
-    
-    sensors.append({'name': "Mppt1 Current", 'sensor_type': "sensor",
-    'register': "mppt_1_current", 'dev_class': "power",
+ 
+    sensors.append({'name': "Power Factor", 'sensor_type': "sensor",
+    'register': "power_factor", 'dev_class': "power",
     'state_class': "measurement"})
-  
+
+    sensors.append({'name': "Grid Frequency", 'sensor_type': "sensor",
+    'register': "grid_frequency", 'dev_class': "power",
+    'state_class': "measurement"})
+
+    sensors.append({'name': "Work state", 'sensor_type': "sensor",
+    'register': "work_state_1", 'dev_class': "power",
+    'state_class': "measurement"})	
+	
+    sensors.append({'name': "Alarm Code", 'sensor_type': "sensor",
+    'register': "alarm_code_1", 'dev_class': "power",
+    'state_class': "measurement"})		
+	
+    sensors.append({'name': "Mppt1 Current", 'sensor_type': "sensor",
+    'register': "mppt_1_current", 'dev_class': "current",
+    'state_class': "measurement"})
+    
+    sensors.append({'name': "Mppt2 Current", 'sensor_type': "sensor",
+    'register': "mppt_2_current", 'dev_class': "current",
+    'state_class': "measurement"})
+
+    sensors.append({'name': "Mppt1 Voltage", 'sensor_type': "sensor",
+    'register': "mppt_1_voltage", 'dev_class': "voltage",
+    'state_class': "measurement"})
+    
+    sensors.append({'name': "Mppt2 Voltage", 'sensor_type': "sensor",
+    'register': "mppt_2_voltage", 'dev_class': "voltage",
+    'state_class': "measurement"})
+	
+	sensors.append({'name': "Total Running time", 'sensor_type': "sensor",
+	'register': "total_running_time", 'dev_class': "time",
+	'state_class': "total_increasing"})
+	
+	sensors.append({'name': "Daily Running time", 'sensor_type': "sensor",
+	'register': "daily_running_time", 'dev_class': "time",
+	'state_class': "total_increasing"})	
+    
 options['exports'][0]['ha_sensors']= sensors
 
 with open('config.sg', 'w') as yaml_file:
