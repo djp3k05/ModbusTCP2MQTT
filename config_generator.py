53adc8328378b774df4f54c38bc4ae6c4c0a4c30
djp3k05 <79905373+djp3k05@users.noreply.github.com>
2023-03-01 17:57:12 +0200

    Update config_generator.py

--- config_generator.py
+++ config_generator.py
@@ -221,17 +221,10 @@ if options['inverter']['level'] >= 2:
     'register': "grid_frequency", 'dev_class': "power",
     'state_class': "measurement"})
 
-    sensors.append({'name': "Working state", 'sensor_type': "binary_sensor",
-    'register': "work_state_1"})
-
     sensors.append({'name': "Work State", 'sensor_type': "binary_sensor",
-    'register': "work_state_1",'dev_class': "running"})
+    'register': "work_state_1"})
 	
-    sensors.append({'name': "Work state orig value", 'sensor_type': "sensor",
-    'register': "work_state_1", 'dev_class': "power",
-    'state_class': "measurement"})	
-
-    sensors.append({'name': "Work state 2", 'sensor_type': "binary_sensor",
+    sensors.append({'name': "Work State 2", 'sensor_type': "binary_sensor",
     'register': "work_state_2"})
 	
     sensors.append({'name': "Alarm Code", 'sensor_type': "sensor",
